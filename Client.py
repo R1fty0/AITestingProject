344a113ada7509e74a32dfedc6d9b97834cbc87c
Mohit Sah <m.sah@surreyschools.ca>
2023-01-05 14:04:00 -0800

    2023-01-05
    - Created Project
    - AI generated Server and Client

---
+++ Client.py
@@ -0,0 +1,30 @@
+import socket
+
+# Set up the client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect(('localhost', 5000))
+
+while True:
+  print("Client is running")
+  # Get a guess from the user
+  guess = input('Enter a number between 1 and 100: ')
+  guess = int(guess)
+
+  # Send the guess to the server
+  client_socket.send(bytes(str(guess), 'utf-8'))
+
+  # Receive the response from the server
+  data = client_socket.recv(1024)
+
+  # Check if the guess was correct
+  if data == b'You guessed correctly!':
+    print('You guessed correctly!')
+    break
+  else:
+    print('You guessed incorrectly. Try again.')
+
+# Close the client socket
+client_socket.close()
+
---
+++ Server.py
@@ -0,0 +1,36 @@
+import random
+import socket
+
+# Set up the server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 5050))
+server_socket.listen()
+
+# Generate a random number for the client to guess
+secret_number = random.randint(1, 100)
+
+while True:
+  print("Server is running")
+  # Accept incoming connections
+  client_socket, client_address = server_socket.accept()
+
+  # Receive data from the client
+  data = client_socket.recv(1024)
+  data = int(data)
+
+  # Check if the received data is the secret number
+  if data == secret_number:
+    # Send a message back to the client to let them know they guessed correctly
+    client_socket.send(b'You guessed correctly!')
+  else:
+    # Send a message back to the client to let them know they guessed incorrectly
+    client_socket.send(b'You guessed incorrectly. Try again.')
+    if data > secret_number:
+      print("Your guess was too low.")
+    elif data < secret_number:
+      print("Your guess was too high.")
+
+
+  # Close the client socket
+  client_socket.close()
+
